67e09fecb0595bac4318f53d761df5e53780ebc8
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-03-28 01:39:28 +0100

    add solution 0

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Implementation of redis python
+"""
+
+import requests
+import redis
+import time
+
+
+def get_page(url: str) -> str:
+    redis_conn = redis.Redis()
+
+    count_key = f"count:{url}"
+    access_count = redis_conn.get(count_key)
+    if access_count:
+        redis_conn.incr(count_key)
+    else:
+        redis_conn.set(count_key, 1)
+        redis_conn.expire(count_key, 10)
+    response = requests.get(url)
+    html_content = response.text
+
+    return html_content
